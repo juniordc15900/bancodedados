1aefece7073dfb5e39daa2d07f64152b722d4df0
Alessandro Junior <juniordc15900@gmail.com>
2021-04-04 00:45:29 -0300

    cadastra usuario backend

--- default-django/projeto/main/admin.py
+++ default-django/projeto/main/admin.py
@@ -1,12 +1,12 @@
 from django.contrib import admin
 from . import models
 
-admin.site.register(models.ContactMessage)
-admin.site.register(models.MainDocument)
-admin.site.register(models.HomePage)
-admin.site.register(models.ContactPage)
-admin.site.register(models.AboutPage)
-admin.site.register(models.SiteDefault)
+admin.site.register(models.Cliente)
+admin.site.register(models.Fornecedor()
+admin.site.register(models.Product)
+admin.site.register(models.Address)
+admin.site.register(models.Card)
+
 
 
 
