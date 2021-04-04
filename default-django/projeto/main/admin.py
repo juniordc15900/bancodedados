b103356291096c2e4e3baa609e9c7f0914a8ff7d
Alessandro Junior <juniordc15900@gmail.com>
2021-04-04 16:21:27 -0300

    models supplier and client

--- default-django/projeto/main/admin.py
+++ default-django/projeto/main/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from . import models
 
 admin.site.register(models.Cliente)
-admin.site.register(models.Fornecedor()
+admin.site.register(models.Fornecedor)
 admin.site.register(models.Product)
 admin.site.register(models.Address)
 admin.site.register(models.Card)
