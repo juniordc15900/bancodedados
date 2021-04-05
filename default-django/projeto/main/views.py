0fddccfaa818e964e0e07337b9c5f45450715014
marcelomcolchete <marcelomcolchete@gmail.com>
2021-04-05 05:40:27 -0300

    login/cadastro já funcionando 100%

--- default-django/projeto/main/views.py
+++ default-django/projeto/main/views.py
@@ -10,6 +10,7 @@ from . import validator_forallz
 from django.contrib.auth.models import User
 from django.contrib.auth import login as dj_login
 from django.contrib.auth import logout as dj_logout
+from django.contrib.auth import authenticate as dj_authenticate
 
 def index(request):
     data = {}
@@ -35,6 +36,18 @@ def login(request):
     data = {}
     data['current_title'] = 'a'
     data['current_description'] = 'b'
+    if request.method == 'POST':
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+        user = dj_authenticate(username=username, password=password)
+        if user is not None:
+            if user.is_active:
+                dj_login(request, user)
+                return render(request,'mains/home1.html',data)
+            else:
+                data["message"] = "Sua conta foi desativada."
+        else:
+            data["message"] = "Seu usuario e senha não batem."
     return render(request,'mains/login.html',data)
 
 def registerClient(request):
@@ -48,18 +61,28 @@ def registerClient(request):
         cpf = request.POST.get("cpf")
         email = request.POST.get("email")
         password = request.POST.get("password")
-        new_client = models.Client(
-            first_name=first_name,
-            last_name=last_name,
-            email=email,
-            cpf=cpf,
-            password=password,
-        )
-        new_client.save()
-        django_user = User(password = password, first_name=first_name, last_name=last_name, username=email, email=email)
-        django_user.save()
-        dj_login(request,django_user)
-    return render(request,'mains/cadastra_cliente.html', data)
+        try:
+            repeat_username = User.objects.get(username__exact=email)
+        except:
+            repeat_username = None
+        print(repeat_username)
+        if repeat_username == None:
+            new_client = models.Client(
+                first_name=first_name,
+                last_name=last_name,
+                email=email,
+                cpf=cpf,
+                password=password,
+            )
+            new_client.save()
+            django_user = User.objects.create_user(password = password, first_name=first_name, last_name=last_name, username=email, email=email)
+            django_user.save()
+            dj_login(request,django_user)
+            return render(request,'mains/home1.html', data)
+        else:
+            print("Essa conta já existe")
+            data['message'] = "O e-mail "+email+" já está cadastrado, por favor escolha algum outro!"
+    return render(request,'mains/cadastra-cliente.html', data)
 
 def registerSupplier(request):
     data = {}
@@ -71,17 +94,24 @@ def registerSupplier(request):
         razao = request.POST.get("razao")
         email = request.POST.get("email")
         password = request.POST.get("password")
-        new_supplier = models.Supplier(
-            cnpj=cnpj,
-            razao=razao,
-            email=email,
-            password=password,
-        )
-        new_supplier.save()
-        django_user = User(password = password, username=email, email=email)
-        django_user.save()
-        dj_login(request,django_user)
+        repeat_username = User.objects.get(username__exact=email)
+        if not repeat_username:
+            new_supplier = models.Supplier(
+                cnpj=cnpj,
+                razao=razao,
+                email=email,
+                password=password,
+            )
+            new_supplier.save()
+            django_user = User(password = password, username=email, email=email)
+            django_user.save()
+            dj_login(request,django_user)
+        else:
+            print("Essa conta já existe")
+            data['message'] = "O e-mail "+email+" já está cadastrado, por favor escolha algum outro!"
+
     return render(request,'mains/cadastra-fornecedor.html', data)
+
 def document(request,type_model=''):
     data = {}
     document = models.MainDocument.objects.get(title=type_model)
